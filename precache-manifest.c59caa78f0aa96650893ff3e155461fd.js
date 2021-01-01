c0c718fd8d1dab4abd8c561ad8ff512faec2f0d0
bogutskii <sl9site@gmail.com>
2021-01-01 11:30:17 -0800

    Updates

---
+++ precache-manifest.c59caa78f0aa96650893ff3e155461fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381eee4fbf949fd5fe89bea072645f71",
+    "url": "/alphabet_for_kids/index.html"
+  },
+  {
+    "revision": "f04ecb54238d6f6a2f51",
+    "url": "/alphabet_for_kids/static/css/2.afc36f9d.chunk.css"
+  },
+  {
+    "revision": "f3b49a277407df3610d6",
+    "url": "/alphabet_for_kids/static/css/main.20695114.chunk.css"
+  },
+  {
+    "revision": "f04ecb54238d6f6a2f51",
+    "url": "/alphabet_for_kids/static/js/2.ddd839a3.chunk.js"
+  },
+  {
+    "revision": "f3b49a277407df3610d6",
+    "url": "/alphabet_for_kids/static/js/main.4d366b71.chunk.js"
+  },
+  {
+    "revision": "68e8304006a2bfd1ba58",
+    "url": "/alphabet_for_kids/static/js/runtime~main.c665cc28.js"
+  }
+]);
