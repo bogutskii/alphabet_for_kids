b91f1dcae485b4dfea74187742f6d78f8313a6ad
bogutskii <sl9site@gmail.com>
2021-02-03 21:56:00 -0800

    -add tamplate for VideoPlaylist  ready for routing

---
+++ src/Comonents/NavBar/Navigation.js
@@ -0,0 +1,7 @@
+
+
+const Navigation = () => {
+    return <div>
+        Navigation
+    </div>
+}
---
+++ src/Comonents/VideoPlaylist.js
@@ -0,0 +1,26 @@
+
+import React from 'react'
+
+
+const VideoPlaylist = () => {
+    const playlist = [{
+        name: 'videoName',
+        videoLink: 'https://www.youtube.com/watch?v=75p-N9YKqNo'
+    }]
+
+    //     < iframe
+    // width = "560"
+    // height = "315"
+    // src = "https://www.youtube.com/embed/75p-N9YKqNo"
+    // frameBorder = "0"
+    // allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    // allowFullScreen > < /iframe>
+
+
+    return <div>
+        <ul>
+            <li></li>
+        </ul>
+    </div>
+
+}
