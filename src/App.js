0e4908b75b56b9c7c86141e1957168eb070c471d
bogutskii <sl9site@gmail.com>
2021-02-04 22:36:34 -0800

    --install routing-dom --Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./styles.css";
 import Alphabet from "./Alphabet.js";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navigation from './Comonents/NavBar/Navigation';
+
 export default function App() {
-  return (
-    <div className="App">
-      <Alphabet />
-    </div>
-  );
+
+    return (
+        <Router>
+            <div className="App">
+                <Navigation/>
+
+                <Alphabet/>
+            </div>
+        </Router>
+    );
 }
--- src/Comonents/NavBar/Navigation.js
+++ src/Comonents/NavBar/Navigation.js
@@ -1,7 +1,12 @@
-
+import React from 'react'
+import {NavLink} from "react-router-dom";
 
 const Navigation = () => {
     return <div>
-        Navigation
+        <NavLink to='/Video'>Video</NavLink>
+        <NavLink to='/Test'>Test</NavLink>
+        <NavLink to='/Stats'>Stats</NavLink>
+        <NavLink to='/About'>About</NavLink>
     </div>
 }
+export default Navigation;
