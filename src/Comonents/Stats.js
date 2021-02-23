ee0c131a032d0807d16fb089ca8cac5a89c6b067
bogutskii <sl9site@gmail.com>
2021-02-22 23:41:18 -0800

    -sorting

--- src/Comonents/Stats.js
+++ src/Comonents/Stats.js
@@ -1,19 +1,20 @@
 import React from "react";
-import "../styles.css";
+
 import Accordion from "./Accordion"
 import {connect} from "react-redux";
 
 
 const Stats = (props)=>{
+    const {stats} = props
         return (
             <div>
                 <h1>Your score!</h1>
-                <Accordion title={"Correct: "+ props.stats.corList.length}
-                           children={props.stats.corList.join(', ')}/>
-                <Accordion title={"Wrong: " + props.stats.incList.length}
-                           children={props.stats.incList.join(', ')}/>
-                <Accordion title={"Skipped: " + props.stats.pasList.length}
-                           children={props.stats.pasList.join(', ')}/>
+                <Accordion title={"Correct: "+ stats.corList.length}
+                           children={stats.corList.join(', ')}/>
+                <Accordion title={"Wrong: " + stats.incList.length}
+                           children={stats.incList.join(', ')}/>
+                <Accordion title={"Skipped: " + stats.pasList.length}
+                           children={stats.pasList.join(', ')}/>
 
             </div>
         )
@@ -24,18 +25,18 @@ const mapStateToProps = (state) => ({
     stats: state.stats,
 
 })
+//
+// const mapDispatchToProps = (dispatch) => ({
+//
+//     changeCreateModal: (value) => dispatch({
+//         type: 'SHOW_OR_HIDE_STATS',
+//         payload: {
+//             value: value
+//         }
+//     }),
+//
+// })
 
-const mapDispatchToProps = (dispatch) => ({
 
-    changeCreateModal: (value) => dispatch({
-        type: 'SHOW_OR_HIDE_STATS',
-        payload: {
-            value: value
-        }
-    }),
-
-})
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(Stats);
+export default connect(mapStateToProps, null)(Stats);
 // export default Stats
--- src/Comonents/TestButtons.js
+++ src/Comonents/TestButtons.js
@@ -6,6 +6,39 @@ import {connect} from "react-redux";
 const TestButtons = (props) => {
 
 
+  let letters = [
+        {letter: "A", words: "Apple", transaction: "[ei]"},
+        {letter: "B", words: "Ball", transaction: "[bi:]"},
+        {letter: "C", words: "Cat", transaction: "[ci:]"},
+        {letter: "D", words: "Dog", transaction: "[di:]"},
+        {letter: "E", words: "Eat", transaction: "[i:]"},
+        {letter: "F", words: "Fox", transaction: "[ef]"},
+        {letter: "G", words: "Grass", transaction: "[ʤi:]"},
+        {letter: "H", words: "Hat", transaction: "[eiʧ]"},
+        {letter: "I", words: "Ice", transaction: "[ai]"},
+        {letter: "J", words: "Jam", transaction: "[ʤei]"},
+        {letter: "K", words: "Kid", transaction: "[kei]"},
+        {letter: "L", words: "Life", transaction: "[el]"},
+        {letter: "M", words: "Mouse", transaction: "[em]"},
+        {letter: "N", words: "Nose", transaction: "[en]"},
+        {letter: "O", words: "Oak", transaction: "[ou]"},
+        {letter: "P", words: "Pet", transaction: "[pi:]"},
+        {letter: "Q", words: "Queen", transaction: "[kju:]"},
+        {letter: "R", words: "Red", transaction: "[a:]"},
+        {letter: "S", words: "Sport", transaction: "[es]"},
+        {letter: "T", words: "Tab", transaction: "[ti:]"},
+        {letter: "U", words: "Up", transaction: "[ju:]"},
+        {letter: "V", words: "Van", transaction: "[vi:]"},
+        {letter: "W", words: "Water", transaction: "[‘dablju:]"},
+        {letter: "X", words: "Xenia", transaction: "[eks]"},
+        {letter: "Y", words: "Yes", transaction: "[wai]"},
+        {letter: "Z", words: "Zebra", transaction: "[zed]"}
+    ]
+    console.log(letters.map((obj,i)=> {...obj, id: i}))
+
+
+
+
     return (
 
         <div>
